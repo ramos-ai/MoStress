2294da0d2a31803de9bca81840d9c7dae046222b
Arturo Souza <arturo.souza@sap.com>
2022-09-13 14:36:02 -0300

    caching predictions

--- models/EvaluateModel.py
+++ models/EvaluateModel.py
@@ -8,23 +8,20 @@ from sklearn.metrics import classification_report, confusion_matrix
 
 class EvaluateModel:
     def __init__(self, validationData, modelName, model = None, classes=["Baseline", "Stress", "Amusement"]):
-        if (not model is None):
-            self.model = model
+        self.model = model
         self.modelName = modelName
         self.featuresValidation = validationData["features"]
         self.targetValidation = validationData["targets"]
         self.classes = classes
-        if (self.model._needEncoding):
-            self.self.targetValidation = [ self.model._wesadThreeClassEncoder[str(label)] for label in validationData["targets"] ]
         self.predictions = None
 
     def _makePredictions(self):
-        self.predictions = self.model._makePredictions(self.featuresValidation)
+        if (self.predictions is None):
+            self.predictions = self.model._makePredictions(self.featuresValidation)
+        return self.predictions
 
     def getClassesPredicted(self):
-        if (self.predictions is None):
-            return [np.argmax(probabilities) for probabilities in self._makePredictions()]
-        return self.predictions
+        return [np.argmax(probabilities) for probabilities in self._makePredictions()]
 
     def _getConfusionMatrix(self):
         return confusion_matrix(self.targetValidation, self.getClassesPredicted())
@@ -33,7 +30,7 @@ class EvaluateModel:
         return pd.DataFrame(self._getConfusionMatrix(), index=self.classes, columns=self.classes)
 
     def printConfusionMatrix(self):
-        font = {'family': 'normal',
+        font = {'family': 'DejaVu Sans',
                 'weight': 'bold',
                 'size': 30}
         plt.rc("font", **font)
